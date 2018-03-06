e0e2e36b19ba8ac3644f8d3dc97a8126fab81f85
NickTrush <nicktroosh@gmail.com>
2018-03-06 22:49:59 +0300

    Switch camera is complete!

--- RobofestCube/Src/main.c
+++ RobofestCube/Src/main.c
@@ -387,6 +387,21 @@ int main(void)
       	}
       }
 
+      switch (CH[7])
+      {
+      	case 0:
+      	{
+      		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_2, GPIO_PIN_RESET);
+      		break;
+      	}
+
+      	case 2048:
+      	{
+      		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_2, GPIO_PIN_SET);
+      		break;
+      	}
+      }
+
       Radio_flag = 0; //Receiving access
     }
 
@@ -737,10 +752,20 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_2, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOF, GPIO_PIN_3|GPIO_PIN_5, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PE2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PF3 PF5 */
   GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_5;
